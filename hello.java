a346ba1b9f280a5f2fe57ac8324bf41747d356e4
VinMacri <114018421+VinMacri@users.noreply.github.com>
2023-09-07 16:07:44 -0400

    hello
    my first java program version 1

---
+++ hello.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class hello {
+
+	public static void main(String[] args) {
+		//CS 176L
+		//Vincent Macri
+		//helloWorld
+		
+		System.out.println("Hello World");
+
+	}
+
+}
