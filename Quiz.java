a3646ce90ff25cd3dde1397f053a54f8d69b00a6
VinMacri <114018421+VinMacri@users.noreply.github.com>
2023-10-24 20:23:27 -0400

    Quiz
    This is the Quiz class for the interface assignment

---
+++ Quiz.java
@@ -0,0 +1,14 @@
+//Vincent Macri
+//CS 176L
+//Interface 
+
+public class Quiz implements Measurable{
+	private int score;
+	
+	public Quiz(int score) {
+		this.score = score;	
+	}
+	public double getMeasure() {
+		return score;
+	}
+}
